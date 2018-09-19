0c375f99eb2b9c6f5694da3beb521ad968f3b8d8
nguyenjenn <july19.jenni@gmail.com>
2018-09-19 15:24:12 -0700

    profile page

--- getaway-client/src/App.js
+++ getaway-client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home/Home';
 import Footer from './components/Footer';
+import Profile from './components/Profile/Profile';
 
 //import logo from './logo.svg';
 import './App.css';
@@ -16,9 +17,10 @@ class App extends Component {
         <BrowserRouter>
             <div id='routes' style={ styles.routeStyle }>
               <Route exact path='/' component={Home} />
+              <Route exact path='/profile' component={Profile}/>
             </div>
         </BrowserRouter>
-       
+
         <div className = 'App-footer'>
           <Footer/>
         </div>
