5a5dfa21b340ab06381bb99709684ed7ef7199ac
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-09-21 15:46:47 -0700

    added payment schema

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -5,9 +5,19 @@ var keys = require('../config/stripe');
 var stripe = require('stripe')(keys.secret_key);
 
 router.post('/pay', function(req, res) {
-    res.json({
-        hello: 'world'
-    })
+    let { amount, currency, source, user_id } = req.body;
+    const destination = {
+        account: "acct_1DCutGGZF8qeVOU9"
+    };
+
+    stripe.charges.create({
+        amount,
+        currency,
+        source,
+        description: ''
+    }, (err, charge) => {
+        
+    });
 })
 
 module.exports = router;
