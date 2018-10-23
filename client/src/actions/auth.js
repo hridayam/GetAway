3dfe3084adc15b7fc4380b0db972ca5ee118068e
hridayam <hridayambakshi2011@gmail.com>
2018-10-23 16:22:55 -0700

    persist user

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,41 +5,46 @@ import axios from 'axios';
 import { LOGIN_USER, LOGOUT_USER, REGISTER_USER } from './types';
 
 export const login = (data) => {
-  return dispatch => {
-    axios.post('http://localhost:3001/users/login', data)
-      .then(res => {
-        dispatch({
-          type: LOGIN_USER,
-          payload: res.data.user
-        });
-      })
-      .catch(err => {
-        console.log(err.response);
-      })
+    return dispatch => {
+        axios.post('http://localhost:3001/users/login', data)
+        .then(res => {
+            localStorage.token = res.data.token;
+            localStorage.data = JSON.stringify(res.data.user);
+            console.log(res.data)
+            dispatch(userLoggedIn({token: res.data.token, data: res.data.user}));
+        })
+        .catch(err => {
+            console.log(err.response);
+        })
     }
 }
 
+export const userLoggedIn = data => ({
+    type: LOGIN_USER,
+    payload: data
+});
+
 export const logout =() => {
     return dispatch => {
-      // localStorage.removeItem('jwtToken');
-      // setAuthToken(false);
-      dispatch({
-        type: LOGOUT_USER,
-      });
+        localStorage.removeItem('token');
+        localStorage.removeItem('data');
+        dispatch({
+            type: LOGOUT_USER,
+        });
     }
 }
 
 export const register = data => {
-  return dispatch => {
-    axios.post('/users/register', data)
-      .then(res => {
-        dispatch({
-          type: REGISTER_USER,
-          payload: res.data.user
+    return dispatch => {
+        axios.post('/users/register', data)
+        .then(res => {
+            dispatch({
+            type: REGISTER_USER,
+            payload: res.data.user
+            });
+        })
+        .catch(err => {
+            console.log(err);
         });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+    }
 }
