16062d0fb57b1733bdb31cf040a9c49a022e6e6f
Tuan Anh Le <32113472+Tulean@users.noreply.github.com>
2018-10-25 16:19:29 -0700

    temp code2

--- client/src/actions/search.js
+++ client/src/actions/search.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 import { SEARCH_HOTELS } from './types';
 
-export const search = (data) => {
+export const search = (city, startDate, endDate, numGuests) => {
     return dispatch => {
-        axios.post('http://localhost:3001/hotels/all_hotels', data)
+        axios.post('http://localhost:3001/hotels/search', { city, startDate, endDate, numGuests })
         .then(res => {
-            
+            dispatch({ type: SEARCH_HOTELS , payload: res.data.hotels })
         })
         .catch(err => {
             console.log(err.response);
-        })
+            })
+        }
     }
-}
