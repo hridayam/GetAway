087a30f92ee9e881f1208b5b0e35b7c8edf7201f
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-10-25 15:50:10 -0700

    finished hotels backend polishing

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -3,25 +3,11 @@ const Hotels = require('../models/hotel').Hotels;
 const express = require('express');
 const router = express.Router();
 
-router.get('/all_hotels', async(req, res) => {
+// get all the hotels from the database
+router.get('/all', async(req, res) => {
     try{
-        Hotels.find({}, (err, hotels) => {
-            if(err){
-                res.status(500).json({
-                    success: false,
-                    msg: err
-                });
-            }
-            else if (hotels) {
-                res.status(200).json({
-                    success: true,
-                    msg: 'Found the hotels!',
-                    hotels
-                })
-            }
-        });
-    }
-    catch(err) {
+        findHotels({}, res);
+    } catch(err) {
         res.status(400).json({
             success: false,
             msg: 'Failed to find any hotels with that name'
@@ -29,32 +15,36 @@ router.get('/all_hotels', async(req, res) => {
     }
 });
 
-router.get('/find_all', async(req,res) => {
+// 
+router.get('/search', async(req,res) => {
     let { city } = req.body;
 
     try {
-        Hotels.find({ city }, (err,hotels) => {
-            if (err) {
-                res.status(500).json({
-                    success: false,
-                    msg: err
-                });
-            }
-            else if (hotels) {
-                res.status(200).json({
-                    success: true,
-                    msg: 'Found the hotels!',
-                    hotels
-                });
-            }
-        });
-    }
-    catch(err) {
+        findHotels({ city }, res);
+    } catch(err) {
         res.status(400).json({
             success: false,
             msg: 'Failed to find any hotels with that address'
         });
     }
 });
+
+const findHotels = (params,res) => {
+    Hotels.find(params, (err,hotels) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                msg: err
+            });
+        }
+        else if (hotels) {
+            res.status(200).json({
+                success: true,
+                msg: 'Found the hotels!',
+                hotels
+            });
+        }
+    });
+}
 
 module.exports = router;
