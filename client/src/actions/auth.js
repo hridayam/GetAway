f9d31b79243225fc031b7cf217a17075d8a63c2a
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-11-30 15:24:24 -0800

    merging master into this

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -26,8 +26,7 @@ const loginUser = (data, dispatch, cb) => {
         })
         .catch(err => {
             console.log(err.response);
-         
-        })
+        });
 };
 
 export const logout =() => {
